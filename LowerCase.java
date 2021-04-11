8418bfeeacd840c4d95ab4cbe1baa4a56d2bdfde
Timur Sinnurow <42872306+AngelHifumi@users.noreply.github.com>
2021-04-11 20:13:37 +0200

    Update LowerCase.java

--- LowerCase.java
+++ LowerCase.java
@@ -2,17 +2,6 @@ import java.util.Stack;
 
 public class LowerCase {
     public static String toLowerCase(String str) {
-        char[] charArray = str.toCharArray();
-
-        for (int i = 0; i < charArray.length; i++) {
-            if(charArray[i] >= 65 && charArray[i] <= 90) {
-                charArray[i] += 32;
-            }
-        }
-        String output = new String(charArray);
-        return output;
-    }
-    public static String toLowerCaseChad(String str) {
         Stack<Character> stack = new Stack<>();
         //String output = new String();
         StringBuilder stringBuilder = new StringBuilder();
